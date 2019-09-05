e463225c8777fed761a725dcfd2c758e17f1b2f7
thiagaraju <54875385+thiagaraju@users.noreply.github.com>
2019-09-05 14:18:07 +0530

    Update bdcrunner_01.java

--- src/test/java/mybdcrunner/bdcrunner_01.java
+++ src/test/java/mybdcrunner/bdcrunner_01.java
@@ -4,7 +4,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin={"pretty","html:target/cucumber-report","json:target/cucumber-JSON-report"},features="C:\\Users\\t.thiagaraju\\Downloads\\New folder\\MyDemo_01_Cucumber\\Feature99",glue ="bdcstepdefenitions"
-)
+@CucumberOptions(plugin = {"json:target/cucumber.json"},features="C:\\Users\\t.thiagaraju\\Downloads\\New folder\\MyDemo_01_Cucumber\\Feature99",glue ="bdcstepdefenitions")
 public class bdcrunner_01 {
 }
